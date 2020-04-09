ce4e7f923827c5c0c4468e23cd28dafee7e8bd83
SolenoidEntity <perrin.j.walker@gmail.com>
2020-04-09 22:06:57 +0100

    bring up to date

--- Codehere.py
+++ Codehere.py
@@ -1,5 +1,10 @@
-Test Test
+from time import sleep
 
-Adding some code to see differences.
+print("This is my file to demonstrate best practices.")
 
-Blah blah.
+def process_data(data):
+    print("Beginning data processing...")
+    modified_data = data + " that has been modified"
+    sleep(3)
+    print("Data processing finished.")
+    return modified_data
---
+++ bestpractices.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env python3
+
+import platform
+
+def main():
+	print("hello")
+	print(platform.python_version())
+
+if __name__ == '__main__':
+	main()
+
+	
